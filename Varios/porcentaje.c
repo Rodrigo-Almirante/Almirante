eeb2d467ae22b7a6994ebdc7b8f4004374b8455f
Rodrigo-Almirante <138017981+Rodrigo-Almirante@users.noreply.github.com>
2023-07-13 16:27:19 -0300

    Add files via upload

--- Varios/porcentaje.c
+++ Varios/porcentaje.c
@@ -3,13 +3,13 @@
 int main() {
    float numero1, numero2, porcentaje,diferencia;
 
-   // Pedir al usuario que ingrese dos nÃºmeros
+   // Pedir al usuario que ingrese dos números
    printf("\nIngrese el primer numero: ");
    scanf("%f", &numero1);
    printf("Ingrese el segundo numero: ");
    scanf("%f", &numero2);
 
-    // Verificar si el primer nÃºmero es mayor que el segundo
+    // Verificar si el primer número es mayor que el segundo
     while (numero1 <= numero2) {
         printf("El segundo numero debe ser menor que el primero. Por favor, ingrese un numero valido.\n");
         printf("Ingrese el segundo numero nuevamente: ");
@@ -24,5 +24,8 @@ int main() {
    // Mostrar el resultado
    printf("\nEl porcentaje entre %.2f y %.2f es: %%%.2f\n", numero1, numero2, porcentaje);
    
+   printf("\n         Presione cualquier tecla para continuar\n");  
+   getch();
+
    return 0;
 }
