9ca4bae5b31ff81479407a2dde96599c0a1dbc9f
Rodrigo-Almirante <138017981+Rodrigo-Almirante@users.noreply.github.com>
2023-07-21 16:49:06 -0300

    Add files via upload

---
+++ Varios/Calculo de inflacion en un periodo.py
@@ -0,0 +1,13 @@
+
+porcentaje = input("\nIngresar el porcentaje mensual (7%): %")
+if not porcentaje:
+  porcentaje = "7"
+
+periodo = input("\nIngresar el Periodo en meses  (12): ")
+if not periodo:
+  periodo = "12"
+
+valor = 1 + float(porcentaje)/100
+valor = ((valor**int(periodo))-1)*100                               # ((1 + Porcentaje Mensual) ^ Periodo - 1) * 100
+
+print("\nCon una inflacion mensual de %"+porcentaje,"se tiene una inflacion en",periodo,"meses de %"+str(valor))
