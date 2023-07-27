184a6ce73e178173e1a851027075993a6a96ccc0
Rodrigo-Almirante <138017981+Rodrigo-Almirante@users.noreply.github.com>
2023-07-26 22:50:20 -0300

    Add files via upload

--- Varios/uptime.py
+++ Varios/uptime.py
@@ -1,4 +1,19 @@
-import ctypes                                                    # Necesario para utilizar la funcion GetTickCount64()
+import os
+
+if os.name == 'nt':                                              # Verifica que se este corriendo en Windows para poder importar la funcion ctypes
+ import ctypes                                                   # Necesario para utilizar la funcion GetTickCount64()
+
+else:
+ with open('/proc/uptime', 'r') as f:                            # Procedimiento para linux
+      uptime_seconds = str(f.readline().split(".")[0])           # Lee el valor que es un flotante pero corta la string cuando encuentra un .
+
+      mins, seg = divmod(int(uptime_seconds), 60)                # Con el valor en segundos, ahora se tienen los minutos
+      horas, mins = divmod(mins, 60)                             # Los cuales se los pasa a horas
+      dias, horas = divmod(horas, 24)                            # y por ultimo en dias
+      print(f"{dias} dias, {horas:02}:{mins:02}:{seg:02}")       # (format = x days, HH:MM:SS)
+
+ exit()
+                                                    
  
 lib = ctypes.windll.kernel32                                     # Toma de las librerias del kernel donde esta GetTickCount64()
  
