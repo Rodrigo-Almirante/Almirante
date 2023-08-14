19eaca40893d572323da85b55d9bdbe6f96932d0
Rodrigo-Almirante <138017981+Rodrigo-Almirante@users.noreply.github.com>
2023-08-14 09:41:33 -0300

    Update and rename porcentaje.c to porcentaje.c

--- Matematica/porcentaje.c
+++ Matematica/porcentaje.c
@@ -3,13 +3,13 @@
 int main() {
    float numero1, numero2, porcentaje,diferencia;
 
-   // Pedir al usuario que ingrese dos números
+   // Pedir al usuario que ingrese dos nÃºmeros
    printf("\nIngrese el primer numero: ");
    scanf("%f", &numero1);
    printf("Ingrese el segundo numero: ");
    scanf("%f", &numero2);
 
-    // Verificar si el primer número es mayor que el segundo
+    // Verificar si el primer nÃºmero es mayor que el segundo
     while (numero1 <= numero2) {
         printf("El segundo numero debe ser menor que el primero. Por favor, ingrese un numero valido.\n");
         printf("Ingrese el segundo numero nuevamente: ");
