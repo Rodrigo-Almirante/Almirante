d822ccbf1a1cadc20999b852b5bbb1f30421f666
Rodrigo-Almirante <138017981+Rodrigo-Almirante@users.noreply.github.com>
2023-10-08 12:17:03 -0300

    Add files via upload

--- C/Local IP alternative.c
+++ C/Local IP alternative.c
@@ -2,31 +2,39 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-int main() {
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("Error al inicializar Winsock\n");
-        return 1;
-    }
+int main()
+{
+ WSADATA wsaData;
+ if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) 
+ {
+  printf("Error al inicializar Winsock\n");
+  return 1;
+ }
 
-    char hostname[256];
-    if (gethostname(hostname, sizeof(hostname)) == SOCKET_ERROR) {
-        printf("Error al obtener el nombre del host\n");
-        WSACleanup();
-        return 1;
-    }
+char hostname[256];
+if (gethostname(hostname, sizeof(hostname)) == SOCKET_ERROR) 
+ {
+  printf("Error al obtener el nombre del host\n");
+  WSACleanup();
+  return 1;
+ }
 
-    struct hostent* host;
-    host = gethostbyname(hostname);
-    if (host == NULL) {
-        printf("Error al obtener la información del host\n");
-        WSACleanup();
-        return 1;
-    }
+printf("\nNombre de esta computadora en la red local: %s\n",hostname);
 
-    char* ip = inet_ntoa(*(struct in_addr*)host->h_addr);
-    printf("Direccion IP local: %s\n", ip);
+struct hostent* host;
+host = gethostbyname(hostname);
+if (host == NULL) 
+ {
+  printf("Error al obtener la información del host\n");
+  WSACleanup();
+  return 1;
+ }
 
-    WSACleanup();
-    return 0;
+
+char* ip = inet_ntoa(*(struct in_addr*)host->h_addr);
+
+printf("Direccion IP local: %s\n", ip);
+
+WSACleanup();
+return 0;
 }
